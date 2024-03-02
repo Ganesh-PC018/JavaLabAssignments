39952075be7717c851971304706dcda98bd67e7c
GaneshPC-018 <2023bit501@sggs.ac.in>
2024-03-02 20:22:23 +0530

    No Main Method

---
+++ google/NoMainMethod.java
@@ -0,0 +1,8 @@
+package google;
+public class NoMainMethod {
+    // This class does not have a main method
+
+    public void someMethod() {
+        System.out.println("This class does not have a main method.");
+    }
+}
