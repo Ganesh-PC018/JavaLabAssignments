749b5544b9410991d5e21427ef7420b74c5247db
GaneshPC-018 <2023bit501@sggs.ac.in>
2024-03-24 22:21:33 +0530

    Program Without Main

---
+++ google/CanWeExecuteProgramWithoutMainMethod.java
@@ -0,0 +1,11 @@
+package google;
+public class CanWeExecuteProgramWithoutMainMethod {
+    static
+    {
+        System.out.println("Hello im Here Without Main Method But i'm Unable Run so after 1.7 JDK i can't execute without main");
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
